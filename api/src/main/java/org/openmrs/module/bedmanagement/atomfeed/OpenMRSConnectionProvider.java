5f101dcbdfda92ca39129b6b81d2f4798662596e
pchandra <pchandra@thoughtworks.com>
2014-01-28 23:11:07 +0530

    praveen|temporary fix for the jdbc connection provider

--- api/src/main/java/org/openmrs/module/bedmanagement/atomfeed/OpenMRSConnectionProvider.java
+++ api/src/main/java/org/openmrs/module/bedmanagement/atomfeed/OpenMRSConnectionProvider.java
@@ -4,13 +4,27 @@ import org.hibernate.SessionFactory;
 import org.ict4h.atomfeed.jdbc.JdbcConnectionProvider;
 import org.ict4h.atomfeed.server.exceptions.AtomFeedRuntimeException;
 import org.openmrs.api.context.ServiceContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
 
 import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class OpenMRSConnectionProvider implements JdbcConnectionProvider {
+    private PlatformTransactionManager transactionManager;
+    private TransactionStatus transactionStatus; // TODO : Mujir/Sush - can this be a field. One instance of bean.
+    private Connection connection;
+
+    @Autowired
+    public OpenMRSConnectionProvider(PlatformTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
+    }
+    //TODO: Remove this and inject transaction manager?
+    public OpenMRSConnectionProvider(){}
+
     @Override
     public Connection getConnection() throws SQLException {
         ServiceContext serviceContext = ServiceContext.getInstance();
@@ -20,7 +34,8 @@ public class OpenMRSConnectionProvider implements JdbcConnectionProvider {
             field.setAccessible(true);
             ApplicationContext applicationContext = (ApplicationContext) field.get(serviceContext);
             SessionFactory factory = (SessionFactory) applicationContext.getBean("sessionFactory");
-            return factory.getCurrentSession().connection();
+            connection = factory.getCurrentSession().connection();
+            return connection;
         } catch (Exception e) {
             throw new AtomFeedRuntimeException(e);
         }
@@ -29,5 +44,34 @@ public class OpenMRSConnectionProvider implements JdbcConnectionProvider {
     @Override
     public void closeConnection(Connection connection) throws SQLException {
         connection.close();
+    }
+    //TODO : fix using transaction manager
+    @Override
+    public void startTransaction() {
+        try {
+            if(connection == null || connection.isClosed())
+                getConnection();
+            connection.setAutoCommit(false);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void commit() {
+        try {
+            connection.commit();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void rollback() {
+        try {
+            connection.rollback();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
