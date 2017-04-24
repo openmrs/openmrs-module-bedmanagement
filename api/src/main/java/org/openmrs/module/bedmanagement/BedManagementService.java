d177b61999103fe3c0d45a98996fb2d76010dac0
Santhosh Bairam <bairamsa@thoughtworks.com>
2017-04-24 14:32:36 +0530

    Santhosh, Pramida | #275 | Authorizing the user for having the Get Tags privilege to get all bed tags

--- api/src/main/java/org/openmrs/module/bedmanagement/BedManagementService.java
+++ api/src/main/java/org/openmrs/module/bedmanagement/BedManagementService.java
@@ -55,6 +55,6 @@ public interface BedManagementService extends OpenmrsService {
     @Authorized(value = {"Get Beds", "Get Admission Locations"}, requireAll=true)
     BedDetails getLatestBedDetailsByVisit(String visitUuid);
 
-    @Authorized(value = {"Get Beds", "Get Admission Locations"}, requireAll=true)
+    @Authorized(value = {"Get Tags"}, requireAll=true)
     List<BedTag> getAllBedTags();
 }
