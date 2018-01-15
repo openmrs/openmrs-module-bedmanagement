36cc08fa1533dc5e33dff8bdffee0b1fd458c52b
SanishMaharjan <sanishmaharjan@lftechnology.com>
2018-01-15 17:13:54 +0545

    BAH-436 Add link to the admin page from the legacy admin UI

---
+++ omod/src/main/java/org/openmrs/module/bedmanagement/extension/html/AdminList.java
@@ -0,0 +1,38 @@
+package org.openmrs.module.bedmanagement.extension.html;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.openmrs.api.context.Context;
+import org.openmrs.module.Extension;
+import org.openmrs.module.web.extension.AdministrationSectionExt;
+
+/**
+ * This class defines the links that will appear on the administration page under the
+ * "bedmanagement.title" heading.
+ */
+public class AdminList extends AdministrationSectionExt {
+	
+	final String KEY_APP_BASE_URL = "owa.appBaseUrl";
+	
+	public Extension.MEDIA_TYPE getMediaType() {
+		return Extension.MEDIA_TYPE.html;
+	}
+	
+	public String getTitle() {
+		return "bedmanagement.title";
+	}
+	
+	/**
+	 * @see AdministrationSectionExt#getLinks()
+	 */
+	public Map<String, String> getLinks() {
+		LinkedHashMap<String, String> map = new LinkedHashMap<String, String>();
+		String appBaseUrl = Context.getAdministrationService().getGlobalProperty(KEY_APP_BASE_URL);
+		map.put(appBaseUrl + "/bedmanagement/admissionLocations.html", "bedmanagement.admissionLocations");
+		map.put(appBaseUrl + "/bedmanagement/bedTypes.html", "bedmanagement.bedTypes");
+		map.put(appBaseUrl + "/bedmanagement/bedTags.html", "bedmanagement.bedTags");
+		return map;
+	}
+	
+}
