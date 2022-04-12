661b0861dd4c58417113fc067552479d99b357a3
rohit-yawalkar <rohit.yawalkar@thoughtworks.com>
2022-04-12 12:36:37 +0530

    BAH-1449 - | Rohit | - Upgrades openmrs version to 2.2.0 and web-services version to 2.23.0

--- omod/src/main/java/org/openmrs/module/bedmanagement/rest/resource/BedDetailsResource.java
+++ omod/src/main/java/org/openmrs/module/bedmanagement/rest/resource/BedDetailsResource.java
@@ -37,7 +37,7 @@ import org.openmrs.module.webservices.rest.web.response.ResponseException;
 import java.util.Arrays;
 
 @Resource(name = RestConstants.VERSION_1 + "/beds", supportedClass = BedDetails.class, supportedOpenmrsVersions = { "1.9.*",
-        "1.10.*", "1.11.*", "1.12.*", "2.0.*", "2.1.*" })
+        "1.10.*", "1.11.*", "1.12.*", "2.0.*", "2.1.*", "2.2.*" })
 public class BedDetailsResource extends DelegatingCrudResource<BedDetails> {
 	
 	@Override
--- omod/src/main/java/org/openmrs/module/bedmanagement/rest/resource/BedResource.java
+++ omod/src/main/java/org/openmrs/module/bedmanagement/rest/resource/BedResource.java
@@ -48,7 +48,7 @@ import org.openmrs.module.webservices.rest.web.response.ResourceDoesNotSupportOp
 import org.openmrs.module.webservices.rest.web.response.ResponseException;
 
 @Resource(name = RestConstants.VERSION_1 + "/bed", supportedClass = Bed.class, supportedOpenmrsVersions = { "1.9.*",
-        "1.10.*", "1.11.*", "1.12.*", "2.0.*", "2.1.*" })
+        "1.10.*", "1.11.*", "1.12.*", "2.0.*", "2.1.*", "2.2.*" })
 public class BedResource extends DelegatingCrudResource<Bed> {
 	
 	@Override
