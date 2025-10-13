004ec4d4868733e156cb4dc3a6efa0d79c8fdba1
jwnasambu <wamalwa1844.com>
2025-10-13 03:44:37 +0300

    Refactor imports

--- api/src/test/java/org/openmrs/module/bedmanagement/BedManagementServiceImplTest.java
+++ api/src/test/java/org/openmrs/module/bedmanagement/BedManagementServiceImplTest.java
@@ -31,7 +31,6 @@ import java.util.HashSet;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThrows;
-import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
