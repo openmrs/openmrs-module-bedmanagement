dcf96d8f2c6ae14c32d41434e8a1fcd05969d1d9
Wikum Weerakutti <wikum@linuxdeveloper.space>
2025-11-04 12:16:43 +0530

    Update tests to use `BaseModuleContextSensitiveTest` instead of `BaseModuleWebContextSensitiveTest`. (#92)

--- api/src/test/java/org/openmrs/module/bedmanagement/aop/VisitWithBedPatientAssignmentSaveHandlerTest.java
+++ api/src/test/java/org/openmrs/module/bedmanagement/aop/VisitWithBedPatientAssignmentSaveHandlerTest.java
@@ -9,7 +9,6 @@ import java.util.Date;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.openmrs.Patient;
 import org.openmrs.Visit;
 import org.openmrs.api.PatientService;
@@ -17,14 +16,10 @@ import org.openmrs.api.VisitService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.bedmanagement.BedDetails;
 import org.openmrs.module.bedmanagement.service.BedManagementService;
-import org.openmrs.web.test.BaseModuleWebContextSensitiveTest;
+import org.openmrs.test.BaseModuleContextSensitiveTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@org.springframework.test.context.ContextConfiguration(locations = {
-        "classpath:TestingApplicationContext.xml" }, inheritLocations = true)
-public class VisitWithBedPatientAssignmentSaveHandlerTest extends BaseModuleWebContextSensitiveTest {
+public class VisitWithBedPatientAssignmentSaveHandlerTest extends BaseModuleContextSensitiveTest {
 	
 	@Autowired
 	private BedManagementService bedManagementService;
