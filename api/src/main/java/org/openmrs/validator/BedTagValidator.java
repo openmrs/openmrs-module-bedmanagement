b9520bd7b42f565a37ecb4bcf7a6ac9f5f9aea17
jwnasambu <wamalwa1844.com>
2025-11-20 18:13:15 +0300

    BED-23: Updated tests to use UUID based BedTag instead of the name-based

--- api/src/main/java/org/openmrs/validator/BedTagValidator.java
+++ api/src/main/java/org/openmrs/validator/BedTagValidator.java
@@ -12,45 +12,44 @@ import org.apache.commons.lang3.StringUtils;
 import org.openmrs.annotation.Handler;
 import org.openmrs.module.bedmanagement.entity.BedTag;
 import org.openmrs.module.bedmanagement.service.BedManagementService;
-import org.openmrs.util.ValidateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
-@Handler(supports = {BedTag.class}, order = 50)
+@Handler(supports = { BedTag.class }, order = 50)
 public class BedTagValidator implements Validator {
-
-    @Autowired
-    private BedManagementService bedManagementService;
-
-    @Override
-    public boolean supports(Class<?> clazz) {
-        return BedTag.class.equals(clazz);
-    }
-
-    @Override
-    public void validate(Object target, Errors errors) {
-        if (!(target instanceof BedTag)) {
-            errors.reject("error.general");
-            return;
-        }
-
-        BedTag tag = (BedTag) target;
-
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "error.name");
-
-        if (StringUtils.isBlank(tag.getName())) {
-            return;
-        }
-
-        BedTag existing = bedManagementService.getBedTagByName(tag.getName().trim());
-        if (existing != null && !existing.getUuid().equals(tag.getUuid())) {
-            if (existing.getDateVoided() == null) {
-                errors.rejectValue("name", "general.error.nameAlreadyInUse");
-            }
-        }
-
-        ValidateUtil.validateFieldLengths(errors, tag.getClass(), "name");
-    }
+	
+	@Autowired
+	private BedManagementService bedManagementService;
+	
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return BedTag.class.equals(clazz);
+	}
+	
+	@Override
+	public void validate(Object target, Errors errors) {
+		if (!(target instanceof BedTag)) {
+			errors.reject("error.general");
+			return;
+		}
+		
+		BedTag tag = (BedTag) target;
+		
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "error.name");
+		
+		if (StringUtils.isBlank(tag.getName())) {
+			return;
+		}
+		
+		BedTag existing = bedManagementService.getBedTagByUuid(tag.getUuid().trim());
+		if (existing != null && !existing.getUuid().equals(tag.getUuid())) {
+			if (existing.getDateVoided() == null) {
+				errors.rejectValue("name", "general.error.nameAlreadyInUse");
+			}
+		}
+		
+		ValidateUtil.validateFieldLengths(errors, tag.getClass(), "name");
+	}
 }
