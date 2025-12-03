8e71d0ad151a3427c6206cab07aac6daef0254ce
jwnasambu <33891016+jwnasambu@users.noreply.github.com>
2025-12-03 23:49:04 +0300

    BED-23: Format BedTagMapService.java for readability

--- api/src/main/java/org/openmrs/module/bedmanagement/service/BedTagMapService.java
+++ api/src/main/java/org/openmrs/module/bedmanagement/service/BedTagMapService.java
@@ -7,19 +7,19 @@ import org.openmrs.module.bedmanagement.entity.BedTag;
 import org.openmrs.module.bedmanagement.entity.BedTagMap;
 
 public interface BedTagMapService extends OpenmrsService {
-
+	
 	@Authorized(value = { "Edit Bed Tags", "Get Bed Tags", "Get Beds" }, requireAll = true)
 	BedTagMap save(BedTagMap bedTagMap);
-
+	
 	@Authorized(value = { "Edit Bed Tags", "Get Bed Tags", "Get Beds" }, requireAll = true)
 	void delete(BedTagMap bedTagMap, String reason);
-
+	
 	@Authorized(value = { "Get Bed Tags", "Get Beds" }, requireAll = true)
 	BedTagMap getBedTagMapByUuid(String bedTagMapUuid);
-
+	
 	@Authorized(value = { "Get Bed Tags", "Get Beds" }, requireAll = true)
 	BedTagMap getBedTagMapWithBedAndTag(Bed bed, BedTag bedTag);
-
+	
 	@Authorized(value = { "Get Bed Tags", "Get Beds" }, requireAll = true)
 	BedTag getBedTagByUuid(String bedTagUuid);
 }
