daea5e32440e85d799ee8a181b9a28a2a5e5c82d
jwnasambu <wamalwa1844.com>
2025-12-03 14:53:43 +0300

    BED-23: Revert the changes

--- api/src/main/java/org/openmrs/module/bedmanagement/service/impl/BedManagementServiceImpl.java
+++ api/src/main/java/org/openmrs/module/bedmanagement/service/impl/BedManagementServiceImpl.java
@@ -1,7 +1,7 @@
 /**
  * The contents of this file are subject to the OpenMRS Public License
  * Version 1.0 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License a
+ * compliance with the License. You may obtain a copy of the License at
  * http://license.openmrs.org
  *
  * Software distributed under the License is distributed on an "AS IS"
